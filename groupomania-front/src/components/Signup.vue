97790e70c6c75dbff16f42cb8241e71b9242906b
Tsuplige <thomassupligeon@gmail.com>
2022-01-06 12:39:07 +0100

    mis a jour du composent signup

--- groupomania-front/src/components/Signup.vue
+++ groupomania-front/src/components/Signup.vue
@@ -6,23 +6,23 @@
             <img id="login__logo" src="../img/icon-left-font-monochrome-black.svg">
         </div>
         <div for="email" class="form-group">
-            <input @keyup="validateEmail" type="email" v-model="BodyEmail" class="form-control" id="lg_email" name="lg_email" placeholder="email" required>
+            <input @keyup="validateEmail" @click="validateEmail" type="email" v-model="BodyEmail" class="form-control" id="lg_email" name="lg_email" placeholder="email" required>
             <span v-if="msg.email" class="ErrorMsg">{{msg.email}}</span>
         </div>
         <div class="form-group">
-            <input type="text" @keyup="validateUsername" v-model="BodyUsername" class="form-control" id="lg_username" name="lg_username" placeholder="username" required>
+            <input type="text" @keyup="validateUsername" @click="validateUsername" v-model="BodyUsername" class="form-control" id="lg_username" name="lg_username" placeholder="username" required>
             <span v-if="msg.username" class="ErrorMsg">{{msg.username}}</span>
         </div>
 
         <div class="form-group">
-            <input type="password" @keyup="validatePassword" v-model="BodyPassword" class="form-control" id="lg_password" name="lg_password" placeholder="password" required>
+            <input type="password" @keyup="validatePassword" @click="validatePassword" v-model="BodyPassword" class="form-control" id="lg_password" name="lg_password" placeholder="password" required>
             <span v-if="msg.password" class="ErrorMsg">{{msg.password}}</span>
         </div>
 
-        <div class="form-group">
+        <!-- <div class="form-group">
             <input @keyup="validatePasswordConfirm" v-model="BodyPasswordConfirm" type="password" class="form-control" id="lg_password--confirm" name="lg_password" placeholder="confirm password">
             <span v-if="msg.passwordConfirme" class="ErrorMsg">{{msg.passwordConfirme}}</span>
-        </div>
+        </div> -->
         <input @click="submit" class="submit--button" type="submit" value="S'inscrire">
 
         <p>Vous avez déja un compte ? <router-link to="/">Se Connecter</router-link> </p>
@@ -36,9 +36,12 @@ export default {
   data () {
     return {
       BodyEmail: '',
+      BodyEmailStatus: false,
       BodyPassword: '',
-      BodyPasswordConfirm: '',
+      BodyPasswordStatus: false,
+      // BodyPasswordConfirm: '',
       BodyUsername: '',
+      BodyUsernameStatus: false,
       ErrorMessage: '',
       msg: []
     };
@@ -47,15 +50,16 @@ export default {
     async submit () {
       console.log('submit');
       console.log(this.BodyEmail);
-      try {
-        await API.signup({ email: this.BodyEmail, password: this.BodyPassword, username: this.BodyUsername });
-        this.$router.push({ name: 'Home' });
-      } catch (err) {
+      if (this.BodyEmailStatus == true && this.BodyPasswordStatus == true && this.BodyUsernameStatus == true) {
+        try {
+          await API.signup({ email: this.BodyEmail, password: this.BodyPassword, username: this.BodyUsername });
+          this.$router.push({ name: 'Home' });
+        } catch (err) {
 
-      }
+        }
+      } else window.alert('données fournis incorrects et/ou inexistantes');
     },
     validateEmail () {
-      let statusInfo = false;
       const validRegex = new RegExp(/^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/g);
       console.log(this.BodyEmail);
       const messageOk = '';
@@ -65,15 +69,13 @@ export default {
       //   : 'Invalid Email Address';
       if (validRegex.test(this.BodyEmail) == true) {
         this.msg.email = messageOk;
-        statusInfo = true;
+        this.BodyEmailStatus = true;
       } else {
         this.msg.email = messageNotOk;
-        statusInfo = false;
+        this.BodyEmailStatus = false;
       }
-      console.log(statusInfo);
     },
     validateUsername () {
-      let statusInfo = false;
       const validRegex = new RegExp(/^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]+$/u);
       console.log(this.BodyUsername);
       const messageOk = '';
@@ -83,40 +85,37 @@ export default {
       //   : 'Invalid Email Address';
       if (validRegex.test(this.BodyUsername) == true) {
         this.msg.username = messageOk;
-        statusInfo = true;
+        this.BodyUsernameStatus = true;
       } else {
         this.msg.username = messageNotOk;
-        statusInfo = false;
+        this.BodyUsernameStatus = false;
       }
-      console.log(statusInfo);
     },
     validatePassword () {
-      let statusInfo = false;
       // const taille = (this.BodyPassword).lenght;
       const messageOk = '';
       const messageNotOk = 'Mots de passe trop court';
       if (this.BodyPassword.length > 8) {
         this.msg.password = messageOk;
-        statusInfo = false;
+        this.BodyPasswordStatus = true;
       } else {
         this.msg.password = messageNotOk;
-        statusInfo = true;
+        this.BodyPasswordStatus = false;
       }
-      console.log(statusInfo);
-    },
-    validatePasswordConfirm () {
-      let statusInfo = false;
-      const messageOk = '';
-      const messageNotOk = 'les Mots de passe doivent être identique';
-      if (this.BodyPassword != this.BodyPasswordConfirm) {
-        this.msg.passwordConfirme = messageNotOk;
-        statusInfo = false;
-      } else {
-        this.msg.passwordConfirme = messageOk;
-        statusInfo = true;
-      }
-      console.log(statusInfo);
-    }
+    }//,
+    // validatePasswordConfirm () {
+    //   let statusInfo = false;
+    //   const messageOk = '';
+    //   const messageNotOk = 'les Mots de passe doivent être identique';
+    //   if (this.BodyPassword != this.BodyPasswordConfirm) {
+    //     this.msg.passwordConfirme = messageNotOk;
+    //     statusInfo = false;
+    //   } else {
+    //     this.msg.passwordConfirme = messageOk;
+    //     statusInfo = true;
+    //   }
+    //   console.log(statusInfo);
+    // }
   }
 };
 </script>
